51b22217e0ccc34792c284462563e5be39fbb1ad
Halid Erdur <haliderdur@gmail.com>
2022-12-23 11:03:24 +0000

    ZeroBank header verification

---
+++ src/test/java/com/cydeo/locators/TC02_ZeroBank_HeaderVerif.java
@@ -0,0 +1,38 @@
+package com.cydeo.locators;
+
+/*
+TC #2: Zero Bank header verification
+1. Open Chrome browser
+2. Go to http://zero.webappsecurity.com/login.html
+3. Verify header text
+Expected: “Log in to ZeroBank”
+ */
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC02_ZeroBank_HeaderVerif {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://zero.webappsecurity.com/login.html");
+
+        String actualHeader = driver.findElement(By.tagName("h3")).getText();
+
+        String expectedHeader = "Log in to ZeroBank";
+
+        if (actualHeader.equals(expectedHeader)) {
+            System.out.println("Header test PASS!");
+        } else {
+            System.out.println("Header test FAILED!!!");
+        }
+
+        driver.close();
+
+    }
+}
